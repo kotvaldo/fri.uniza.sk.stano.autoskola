e9a4386a3089d122ba5ea4fd08b5edff504589f5
kotvaldo <95977497+kotvaldo@users.noreply.github.com>
2023-06-05 14:28:54 +0200

    Init

---
+++ app/src/main/java/com/example/myapplication/autoskola3/SylabusActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.autoskola3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SylabusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sylabus)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/autoskola3/VideoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.autoskola3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
