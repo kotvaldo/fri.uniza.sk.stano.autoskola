9df4d5dda14581a367a6f90c0e1a1d68c7b88592
kotvaldo <95977497+kotvaldo@users.noreply.github.com>
2023-06-26 09:36:25 +0200

    Init

--- app/src/main/java/com/example/myapplication/autoskola3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/autoskola3/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.example.myapplication.autoskola3
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.autoskola3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
